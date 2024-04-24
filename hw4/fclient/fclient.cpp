a5c8865c7c663a288b9d09d185439e2a2907ed95
sebjink <sjk4203@gmail.com>
2024-04-23 21:19:25 -0500

    Update client to  receive number of bytes from server; incomplete

--- hw4/fclient/fclient.cpp
+++ hw4/fclient/fclient.cpp
@@ -15,18 +15,18 @@
 
 int main(int argc, char* argv[])
 {
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    struct hostent * server;
-    FILE * fp;
+    int sockfd; // the integer socket we use for this project
+    struct sockaddr_in serv_addr; //server address we are going to 
+    struct hostent * server; // server object we are working with
+    FILE * fp; // file pointer holding the actual binary file we want
     FILE * sockfp;
-    const char* hostname = argv[1];
-    int port = std::atoi(argv[2]);
-    const char* filename = argv[2];
+    const char* hostname = argv[1]; // grab the hostname
+    int port = std::atoi(argv[2]); // grab the port number
+    const char* filename = argv[2]; //grab the file name
 
-    char buffer[1024];
+    char buffer[1024]; //   set up input buffer to read 1024 bytes at a time
 
-    if (argc != 4)
+    if (argc != 4) // make sure the number of args is correct
         std::cerr << "Usage: " << argv[0] << " <hostname> <port> <filename> " << std::endl; return 0; // finish the program if entered wrong
 
     sockfd = (AF_INET, SOCK_STREAM, 0); //setup the socket
@@ -35,22 +35,22 @@ int main(int argc, char* argv[])
     
     server = gethostbyname((const char*)argv[1]); // get the server client address and open a connection to it
 
-    if (server == NULL)
+    if (server == NULL) //check if the server is there
     {
         std::cerr << "ERROR: No such server: " << hostname << std:: endl;
-        close(sockfd);
+        close(sockfd); //close the socket if the server ain't there
         return 1; 
     }
 
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    memcpy(&serv_addr.sin_addr, server->h_addr, server->h_length);
-    serv_addr.sin_port = htons(port);
+    bzero((char *)&serv_addr, sizeof(serv_addr));//fill in the server address with 0s
+    serv_addr.sin_family = AF_INET; //set the sin_family to AF_INET
+    memcpy(&serv_addr.sin_addr, server->h_addr, server->h_length); //copy the sin_addrr to the server's addr
+    serv_addr.sin_port = htons(port);// use htons to set up the port
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0);
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0); //connect using the serv_addr
     {
         std::cerr << "ERROR: Connection Failed: " << strerror(errno) << std::endl;
-        close(sockfd);
+        close(sockfd); //close the socket if we didn't actually connect
         return 1;
     }
 
@@ -59,14 +59,20 @@ int main(int argc, char* argv[])
     strcat(gMessage, "\n");
     //strcat(gMessage, "\0"); // just in ccase we need to null terminate
 
-    if (send(sockfd, gMessage, sizeof(gMessage), 0) < 0)
+    if (send(sockfd, gMessage, sizeof(gMessage), 0) < 0) //send a message with a "g <filename>\n"
     {
         std::cerr << "ERROR: Writing to socket: " <<  strerror(errno) << std::endl;
+        close(sockfd);// close the socket if we cannot send the message
+        return 1;
+    }
+    int fsize;
+    if (sscanf(buffer, "s %d\n", &fsize) != 1)
+    {
+        std::cerr << "ERROR: Failed to Recieve Byte Size/Server Response: " << buffer << std::endl;
         close(sockfd);
         return 1;
     }
-
-    fp = fopen(filename, "wb");
+    fp = fopen(filename, "wb"); //open the filename that is retrieved
     if (fp == NULL)
     {
         std::cerr << "ERROR: Opening File" << std::endl;
@@ -82,20 +88,29 @@ int main(int argc, char* argv[])
         close(sockfd);
         return 1;
     }
-    //std::vector<char*> buffer;
-    char buffer[1024];
-    //bzero(buffer, 1024);
+    /*
     while (fgets(buffer, sizeof(buffer), fp) != nullptr)
     {
         //check for error
         fwrite(buffer, sizeof(char), strlen(buffer), fp);
         bzero(buffer, 1024);        
     }
+    */
 
+    int receievedBytes = 0;
+    while (receievedBytes < fsize)
+    {
+        if (fgets(buffer, sizeof(buffer), sockfp) == NULL)
+            break;
+        int numToWrite = std::min((fsize - receievedBytes), (int)strlen(buffer));
+        fwrite(buffer, sizeof(buffer), numToWrite, fp);
+        receievedBytes += numToWrite;
+    }
+    fclose(fp);
     if (ferror(sockfp))
     {
         std::cerr << "ERROR: Reading from Socket" << std::endl;
-        fclose(fp);
+        //fclose(fp);
         fclose(sockfp);
         return 1;
     }
@@ -103,12 +118,12 @@ int main(int argc, char* argv[])
     if (send(sockfd, "d\n", strlen("d\n"), 0) < 0)
     {
         std::cerr << "ERROR: Failed to send finish" << std::endl;
-        fclose(fp);
+        //fclose(fp);
         fclose(sockfp);
         return 1;
     }
 
-    fclose(fp);
+    //fclose(fp);
     fclose(sockfp);
 
     return 0;
